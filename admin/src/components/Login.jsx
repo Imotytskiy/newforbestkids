024680e1d87559170e2ca1ad68a16ea3a0cea4a0
Igor Motytskiy <imotytskiy@gmail.com>
2025-02-02 00:02:50 +0100

    add norm pass and login

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -14,6 +14,7 @@ const Login = ({ setToken }) => {
         email,
         password,
       });
+      console.log(response);
 
       if (response.data.success) {
         setToken(response.data.token);
