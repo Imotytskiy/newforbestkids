9050ab491167961d442d01179bbc3249a425cc5a
Igor Motytskiy <imotytskiy@gmail.com>
2025-02-01 23:45:31 +0100

    trash

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,29 +1,43 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./components/Navbar"; // Переконайся, що файл існує
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"; // Додай стилі для тостів
-import Admin from "./pages/Admin";
+import "react-toastify/dist/ReactToastify.css";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Add from "./components/Add";
+import List from "./components/List";
+import Orders from "./components/Orders";
 import Login from "./components/Login";
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
-  const [token, setToken] = useState(
-    localStorage.getItem("token") ? localStorage.getItem("token") : ""
-  );
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
+
   useEffect(() => {
     localStorage.setItem("token", token);
   }, [token]);
 
   return (
     <div>
-      {token === "" ? ( // Виправлено з "tokek"
+      {token === "" ? (
         <Login setToken={setToken} />
       ) : (
         <>
           <ToastContainer autoClose={4000} />
           <Navbar setToken={setToken} />
-          <Admin />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
         </>
       )}
     </div>
